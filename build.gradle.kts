e3ae0903948769b409543cde3f4b445f5ce8204a
Joseph Malone <malone.joseph@gmail.com>
2025-04-28 20:43:04 -0700

    initial

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,15 @@
+// build.gradle.kts (Kotlin Project)
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+// No need for SourceSetContainer import anymore
 
 plugins {
+    kotlin("jvm") version "2.1.0"
+    id("org.jetbrains.compose") version "1.7.3"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0" // Add serialization plugin
 
-
-    kotlin("jvm")
-    id("org.jetbrains.compose")
-    id("org.jetbrains.kotlin.plugin.compose")
-//    id("org.web3j") version "4.14.0"
-
+    // NO org.web3j plugin
 }
-
-//web3j {
-//    generatedPackageName.set("io.tictactoe.contracts")
-//    generatedFilesBaseDir.set(file("$buildDir/generated/sources/web3j"))
-//    solcVersion.set("0.8.28")
-//}
 
 group = "vision.salient"
 version = "1.0-SNAPSHOT"
@@ -23,28 +18,36 @@ repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
-
     gradlePluginPortal()
 }
 
-
-
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3") // Or latest version
     implementation(compose.desktop.currentOs)
-    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1") // Or check for the latest version
-
-    implementation("org.web3j:core:5.0.0") // Web3j for Ethereum interaction
+    // Dotenv for loading .env files
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+    // Web3j core library ONLY
+    implementation("org.web3j:core:5.0.0")
+    // Coroutines for async operations
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    // Optional: Logging implementation
+    // implementation("ch.qos.logback:logback-classic:1.3.11")
 }
+
+// REMOVE or comment out the sourceSets block if you added it before
+/*
+sourceSets {
+    main {
+        java {
+            srcDirs("src/main/java")
+        }
+    }
+}
+*/
 
 compose.desktop {
     application {
-        mainClass = "vision.salient.MainKt"
-
+        mainClass = "vision.salient.MainKt" // Matches package
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "tic_tac_toe_compose"
